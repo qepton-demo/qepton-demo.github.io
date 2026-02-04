bb135f9cb260c185adfe1677585ba8c46d3dca87
wriver4 <wriver4@users.noreply.github.com>
2026-02-04 23:08:28 +0000

    Deploy demo app - 9f152361090b272189e55891825404d6d3c7de58

---
+++ assets/AuthLayout-DT5fAqQc.js
@@ -0,0 +1,1 @@
+import{b as a,c as r}from"./QLayout-BI51n2Dy.js";import{d as n,aF as s,q as _,t as c,v as e,l as t}from"./index-B1c2aqA-.js";const m=n({__name:"AuthLayout",setup(u){return(p,l)=>{const o=s("router-view");return _(),c(r,{view:"hHh lpR fFf"},{default:e(()=>[t(a,null,{default:e(()=>[t(o)]),_:1})]),_:1})}}});export{m as default};
